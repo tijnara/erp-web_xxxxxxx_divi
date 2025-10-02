20dcde5f01cf296bb0f51680fab49ecaaa382d3d
Office PC <ajsiapno60@men2corp.com>
2025-10-02 10:46:54 +0800

    erp system initial push dashboard and navigation

--- vos-erp/next.config.ts
+++ vos-erp/next.config.ts
@@ -3,5 +3,15 @@ const nextConfig = {
     turbopack: {
         root: __dirname, // force Turbopack root to this folder
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: '100.119.3.44',
+                port: '8090',
+                pathname: '/assets/**',
+            },
+        ],
+    },
 };
 export default nextConfig;
